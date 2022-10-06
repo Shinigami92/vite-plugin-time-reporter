6e3d678e8efa13b860522be39b0b99cc0d974445
reslear <reslear@gmail.com>
2022-10-07 00:04:20 +0200

    refactor: resolve edits

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'vite-plugin-time-reporter',
       fileName: 'index',
       formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
     },
     minify: false,
     target: 'esnext',
